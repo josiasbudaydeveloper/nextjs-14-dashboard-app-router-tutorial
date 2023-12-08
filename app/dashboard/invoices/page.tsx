3c9933ce2f2b05777ab34fa83f74189de5a98e2d
Josias Buday Dias <josiasbudaydeveloper@gmail.com>
2023-12-08 01:12:23 -0300

    Chapter 12 Mudating Data Able to Edit and Delete invoices

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,6 +6,8 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
+import Link from 'next/link';
+import { revalidatePath } from 'next/cache';
  
 export default async function Page({
   searchParams,
@@ -29,7 +31,7 @@ export default async function Page({
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
